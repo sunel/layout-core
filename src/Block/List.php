1a7f01e71f3c3a5f004c570603126e746b7276fc
Sunel Tr <sunelbe@gmail.com>
2016-10-02 23:31:19 +0530

    added few common blocks

---
+++ src/Block/List.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Layout\Core\Block;
+
+class List extends Text
+{
+    protected function _toHtml()
+    {
+        $this->setText('');
+        foreach ($this->getSortedChildren() as $name) {
+            $block = $this->getLayout()->getBlock($name);
+            if (!$block) {
+                throw new \Layout\InvalidBlockException('Invalid block type:'.$block);
+            }
+            $this->addText($block->toHtml());
+        }
+
+        return parent::_toHtml();
+    }
+}
